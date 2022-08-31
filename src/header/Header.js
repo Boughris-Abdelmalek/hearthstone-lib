5b04bca7ed95aad2b21cf1c9fe832f2bdde4fd4e
malik <boughris@outlook.fr>
2022-08-31 23:30:05 +0100

    setting up the settings component

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.scss';
 import logo from './img/hearthstone-logo.png';
 import settings from './img/settings.png';
 import { Link } from 'react-router-dom';
+import Settings from './settings/settings';
 
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    if (open === false) {
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  }
+
   return (
     <header className={styles.header}>
             <Link to='/' className={styles.logoContainer}>
@@ -13,7 +24,10 @@ const Header = () => {
             </Link>
             <h1 className={styles.title}>Card Visualizer</h1>
             <div className={styles.settings}>
-                <img src={settings} alt='settings' className={styles.settingsImg}></img>
+                <img src={settings} alt='settings' className={styles.settingsImg} onClick={handleClick}></img>
+                {open &&
+                  <Settings />
+              }
             </div>
         </header>
   )
