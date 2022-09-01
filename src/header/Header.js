32c7773e018dcbcf20d6a7f58353ceb942b6b6c2
malik <boughris@outlook.fr>
2022-09-01 22:15:42 +0100

    fixing the return component

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import styles from './Header.module.scss';
-import logo from './img/hearthstone-logo.png';
-import settings from './img/settings.png';
-import { Link } from 'react-router-dom';
-import Settings from './settings/Settings';
-
+import React, { useState } from "react";
+import styles from "./Header.module.scss";
+import logo from "./img/hearthstone-logo.png";
+import settings from "./img/settings.png";
+import { Link } from "react-router-dom";
+import Settings from "./settings/Settings";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -15,22 +14,25 @@ const Header = () => {
     } else {
       setOpen(false);
     }
-  }
+  };
 
   return (
-    <header className={styles.header}>
-            <Link to='/' className={styles.logoContainer}>
-                <img src={logo} alt='logo' className={styles.logo}></img>
-            </Link>
-            <h1 className={styles.title}>Card Visualizer</h1>
-            <div className={styles.settings}>
-                <img src={settings} alt='settings' className={styles.settingsImg} onClick={handleClick}></img>
-                {open &&
-                  <Settings />
-              }
-            </div>
-        </header>
-  )
-}
+    <header className={`${styles.header} header-container`}>
+      <Link to="/" className={styles.logoContainer}>
+        <img src={logo} alt="logo" className={styles.logo}></img>
+      </Link>
+      <h1 className={styles.title}>Card Visualizer</h1>
+      <div className={styles.settings}>
+        <img
+          src={settings}
+          alt="settings"
+          className={styles.settingsImg}
+          onClick={handleClick}
+        ></img>
+        {open && <Settings />}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/home/Home.js
+++ src/home/Home.js
@@ -8,6 +8,11 @@ import SearchBar from "../SearchBar/SearchBar";
 
 const Home = () => {
   const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    open === false ? setOpen(true) : setOpen(false);
+  };
 
   function refreshClock() {
     setDate(new Date());
@@ -34,16 +39,28 @@ const Home = () => {
           <Link to="/quality" className={styles.choiceButton}>
             Quality
           </Link>
-          <Link to="/race" className={styles.choiceButton}>Race</Link>
-          <Link to="/class" className={styles.choiceButton}>Class</Link>
-          <Link to="/set" className={styles.choiceButton}>Set</Link>
+          <Link to="/race" className={styles.choiceButton}>
+            Race
+          </Link>
+          <Link to="/class" className={styles.choiceButton}>
+            Class
+          </Link>
+          <Link to="/set" className={styles.choiceButton}>
+            Set
+          </Link>
         </div>
       </main>
       <footer className={styles.footer}>
         <div className={styles.user}>
-          <img src={user} alt="settings" className={styles.userImg}></img>
+          <img
+            src={user}
+            alt="settings"
+            className={styles.userImg}
+            onClick={handleClick}
+          ></img>
+          {open && <h5 className="copy-right">Abdelmalek Boughris ©️ 2022</h5>}
         </div>
-        <div>
+        <div className={styles.clock}>
           <h1>{date.toLocaleTimeString()}</h1>
         </div>
       </footer>
